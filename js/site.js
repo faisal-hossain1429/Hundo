2319db935ad522cf48d2598343ad861576946827
faisal-hossain1429 <121063890+faisal-hossain1429@users.noreply.github.com>
2023-08-01 01:52:00 -0400

    App Completed
    Documentation and links not completed yet

--- js/site.js
+++ js/site.js
@@ -1,5 +1,45 @@
-//Call function hellow world
+function getValue(){
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    let numbers = [];
 
-function HelloWorld(){
-    alert("hello world");
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)){
+        numbers = generateNumbers(startValue, endValue);
+        displayNumbers(numbers);
+
+    } else {
+        alert("You must enter an integer");
+    }
+
 }
+
+function generateNumbers(startValue, endValue){
+
+    let numbers = [];
+
+    for (let index = startValue; index <= endValue; index++) {
+
+        numbers.push(index);
+    }
+
+    return numbers
+}
+
+
+function displayNumbers(numbers) {
+    let templateRow = "";
+    for (let index = 0; index < numbers.length; index++) {
+      let number = numbers[index]; 
+
+        if (number % 2 === 0) {
+            templateRow += `<tr><td><strong>${number}</strong></td></tr>`;
+        } else {
+            templateRow += `<tr><td>${number}</td></tr>`;
+        }
+
+    }
+    document.getElementById("results").innerHTML = templateRow;
+  }
